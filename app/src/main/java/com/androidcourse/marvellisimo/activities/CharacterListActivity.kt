855f2fc31b5c6041f1a5b46eb0aa1a8eb6a84f32
MikaelFeher <mikael.feher@gmail.com>
2019-02-14 11:28:18 +0100

    Updates character_list_item layout.

--- app/src/main/java/com/androidcourse/marvellisimo/activities/CharacterListActivity.kt
+++ app/src/main/java/com/androidcourse/marvellisimo/activities/CharacterListActivity.kt
@@ -2,6 +2,7 @@ package com.androidcourse.marvellisimo.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.androidcourse.marvellisimo.R
 import com.androidcourse.marvellisimo.services.character.CharacterServiceHandler
 import kotlinx.android.synthetic.main.activity_character_list.*
@@ -13,5 +14,9 @@ class CharacterListActivity : AppCompatActivity() {
         setContentView(R.layout.activity_character_list)
 
         CharacterServiceHandler.getAllCharacters(character_list)
+
+        val characterListItem = findViewById<View>(R.id.character_list_item)
+
+//        characterListItem.setOnClickListener(view -> )
     }
 }
