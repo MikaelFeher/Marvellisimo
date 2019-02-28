83b67b1cd710fc1f7a550477c5646fd222d62d05
MikaelFeher <mikael.feher@gmail.com>
2019-02-28 20:22:20 +0100

    Updates the Favourite class to handle thumbnails.

--- app/src/main/java/com/androidcourse/marvellisimo/models/Favourite.kt
+++ app/src/main/java/com/androidcourse/marvellisimo/models/Favourite.kt
@@ -1,9 +1,11 @@
 package com.androidcourse.marvellisimo.models
 
+import com.androidcourse.marvellisimo.models.character.Thumbnail
 import io.realm.RealmObject
 
 open class Favourite: RealmObject() {
     var name: String? = null
     var id: Int? = null
     var isCharacter: Boolean? = false
+    var thumbnail: Thumbnail? = null
 }
